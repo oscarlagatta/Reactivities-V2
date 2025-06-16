2d8a25539724a693a53cf76ca6adb92b7dc7ce0d
Oscar Lagatta <oscarlagatta@gmail.com>
2025-06-16 22:48:28 +0100

    section5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,32 +1,44 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-  const [activitites, setActivities] = useState<Activity[]>([]);
-  
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  }
+
+
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
       .then(response => setActivities(response.data));
 
-      return () => {}
+    return () => { }
   }, []);
 
   return (
-    <>
+    <Box sx={{bgcolor: '#eeeeee'}} >
       {/* <Typography variant='h3'>Reactivities</Typography> */}
+      <CssBaseline />
       <NavBar />
-      <List>
-        {activitites.map((activity) => (
-          <ListItem key={activity.id}>
-            <ListItemText>
-              {activity.title}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
-    </>
+      <Container maxWidth='xl' sx={{ mt: 3 }}>
+        <ActivityDashboard         
+          activities={activities} 
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+        />
+      </Container>
+
+    </Box>
   );
 }
 export default App
---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,31 @@
+import { Grid2 } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetail";
+
+type Props = {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    selectedActivity?: Activity;
+}
+
+export default function ActivityDashboard({
+    activities,
+    cancelSelectActivity,
+    selectActivity,
+    selectedActivity }: Props) {
+    return (
+        <Grid2 container spacing={3}>
+            <Grid2 size={7}>
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity} />
+            </Grid2>
+            <Grid2 size={5}>
+                {selectedActivity && <ActivityDetail activity={selectedActivity}
+                    cancelSelectActivity={cancelSelectActivity}
+                />}
+            </Grid2>
+        </Grid2>
+    )
+}

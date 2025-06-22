7a1f5a2112dc9c6308193c4fff76173c654806de
Oscar Lagatta <oscarlagatta@gmail.com>
2025-06-22 21:42:44 +0100

    eof 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,75 +1,60 @@
-import { Box, Container, CssBaseline } from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import {Box, Container, CssBaseline, Typography} from "@mui/material";
+import {useState} from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import {useActivities} from "../../lib/hooks/useActivities.ts";
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
+    const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+    const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    axios.get<Activity[]>('https://localhost:5001/api/activities')
-      .then(response => setActivities(response.data));
+    const {activities, isPending } = useActivities()
 
-    return () => { }
-  }, []);
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  const handleCancelSelectActivity = () => {
-    setSelectedActivity(undefined);
-  }
-
-  const handleOpenForm = (id?: string) => {
-    if (id) handleSelectActivity(id);
-    else handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  const handleFormClose = () => {
-    setEditMode(false);
-  }
-
-  const handleSubmitForm = (activity: Activity) => {
-    // creating an activity we check for the id
-    if (activity.id) {
-      setActivities((activities.map( x => x.id === activity.id ? activity : x)))
-    } else {
-      const newActivity = {...activity, id: activities.length.toString()}
-      setSelectedActivity(newActivity);
-      setActivities([...activities, newActivity])
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities!.find(x => x.id === id));
     }
-    setEditMode(false);
-  }
 
-  const handleDelete =( id: string) => {
-    setActivities(activities.filter(x => x.id !== id));
-  }
+    const handleCancelSelectActivity = () => {
+        setSelectedActivity(undefined);
+    }
 
-  return (
-    <Box sx={{bgcolor: '#eeeeee'}} >
-      {/* <Typography variant='h3'>Reactivities</Typography> */}
-      <CssBaseline />
-      <NavBar openForm={handleOpenForm}/>
-      <Container maxWidth='xl' sx={{ mt: 3 }}>
-        <ActivityDashboard         
-          activities={activities} 
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSubmitForm}
-          deleteActivity={handleDelete}
-        />
-      </Container>
+    const handleOpenForm = (id?: string) => {
+        if (id) handleSelectActivity(id);
+        else handleCancelSelectActivity();
+        setEditMode(true);
+    }
 
-    </Box>
-  );
+    const handleFormClose = () => {
+        setEditMode(false);
+    }
+
+
+    return (
+        <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh'}}>
+            {/* <Typography variant='h3'>Reactivities</Typography> */}
+            <CssBaseline/>
+            <NavBar openForm={handleOpenForm}/>
+            <Container maxWidth='xl' sx={{mt: 3}}>
+                {!activities || isPending ? (
+                        <Typography>Loaing...</Typography>
+                    ) :
+                    <ActivityDashboard
+                        activities={activities}
+                        selectActivity={handleSelectActivity}
+                        cancelSelectActivity={handleCancelSelectActivity}
+                        selectedActivity={selectedActivity}
+                        editMode={editMode}
+                        openForm={handleOpenForm}
+                        closeForm={handleFormClose}
+
+
+                    />
+                }
+
+            </Container>
+
+        </Box>
+    );
 }
+
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,22 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import App from './app/layout/App.tsx';
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 
-import App from './app/layout/App.tsx'
+const queryClient = new QueryClient();
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+    </QueryClientProvider>
+    
   </StrictMode>,
 )
